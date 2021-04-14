d695a97fcd129ed97690957aba833223fed54db9
Vaibhav Pothireddy <vp373@njit.edu>
2021-04-14 17:00:11 -0400

    Revert "Task: Assignment 2- Added code to app.py"
    This reverts commit 7127e46f

--- app/app.py
+++ app/app.py
@@ -2,7 +2,6 @@ from typing import List, Dict
 import mysql.connector
 import simplejson as json
 from flask import Flask, Response
-from flask import render_template
 
 app = Flask(__name__)
 
@@ -28,15 +27,7 @@ def cities_import() -> List[Dict]:
 
 
 @app.route('/')
-def index():
-    user = {'username': 'Miguel'}
-    cities_data = cities_import()
-
-    return render_template('index.html', title='Home', user=user, cities=cities_data)
-
-
-@app.route('/api/cities')
-def cities() -> str:
+def index() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
