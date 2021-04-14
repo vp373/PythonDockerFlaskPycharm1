caeeb814e16782b424b9304b6a59cd98bd4c4ab7
Vaibhav Pothireddy <vp373@njit.edu>
2021-04-14 17:22:31 -0400

    Task: Added new code from github repo

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 import mysql.connector
 import simplejson as json
 from flask import Flask, Response
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -27,11 +28,19 @@ def cities_import() -> List[Dict]:
 
 
 @app.route('/')
-def index() -> str:
+def index():
+    user = {'username': 'Miguel'}
+    cities_data = cities_import()
+
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
+
+
+@app.route('/api/cities')
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    app.run(host='0.0.0.0')
